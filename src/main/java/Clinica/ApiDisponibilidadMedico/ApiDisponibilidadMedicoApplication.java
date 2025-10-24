7e55028b8d0971077c3f9c221871dbe820e214f3
Kevin Broncano <kevinbroncanop@gmail.com>
2025-10-24 10:47:30 -0500

    Creación de ApiDisponibilidadMedico

---
+++ src/main/java/Clinica/ApiDisponibilidadMedico/ApiDisponibilidadMedicoApplication.java
@@ -0,0 +1,13 @@
+package Clinica.ApiDisponibilidadMedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDisponibilidadMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDisponibilidadMedicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Clinica/ApiDisponibilidadMedico/ApiDisponibilidadMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package Clinica.ApiDisponibilidadMedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDisponibilidadMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
